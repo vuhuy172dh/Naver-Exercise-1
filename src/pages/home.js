a5bea00828b791d53d29d6bef04d1307c9857534
Vu_Huy <quochuy123dh@gmail.com>
2022-09-26 11:35:56 +0700

    add tailwindcss

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <div>Hello World</div>
+  return <div className="font-mont">Hello World</div>
 }
 
 export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './src/pages/*.{js,ts,jsx,tsx}',
+    './src/*.{js,jsx,ts,tsx}',
+    './src/components/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    fontFamily: {
+      mont: ['Montserrat', 'sans-serif'],
+      poppins: ['Poppins', 'sans-serif'],
+      roboto: ['Roboto', 'sans-serif']
+    },
+    extend: {}
+  },
+  plugins: []
+}
